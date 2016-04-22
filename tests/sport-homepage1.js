f8fe3cdf0b4cc78426d6f113f4eedfefdd4b3dbc
Paul Hammond <paul.hammond@bbc.co.uk>
2016-04-22 14:48:19 +0100

    updating tests

--- tests/sport-homepage1.js
+++ tests/sport-homepage1.js
@@ -1,14 +1,25 @@
 module.exports = {
-    'Test homepage' : function (browser) {
-        browser
-            .url('http://www.bbc.co.uk/sport')
-            .resizeWindow(1000, 800)
-            .waitForElementVisible('.primary-nav', 1000)
+    before: function (browser) {
+        browser.url('http://www.bbc.co.uk/sport')
+            .resizeWindow(1000, 800);
+    },
+
+    'Morph live scores': function (browser) {
+        browser.waitForElementVisible('.primary-nav', 1000)
             .waitForElementVisible('button.sp-c-filter-nav__button', 1000)
-            .click('button.sp-c-filter-nav__button')
             .pause(1000)
             .assert.elementPresent('#morph-live-scores')
             .assert.containsText('#morph-live-scores', 'Football Scores')
+
+    },
+
+    'Headlines': function (browser) {
+        browser.assert.elementPresent('.anfield');
+    },
+
+    'Audio & Video': function(browser) {
+        browser.assert.elementPresent('#audio-video')
+            .assert.containsText('#audio-video', 'Audio & Video')
             .end();
     }
 };
