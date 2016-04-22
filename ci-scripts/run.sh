f7cfef1dbb3f5bb08894ec7e6a08dd971d32b579
Paul Hammond <paul.hammond@bbc.co.uk>
2016-04-22 09:22:57 +0100

    adding capacity for retries

--- ci-scripts/run.sh
+++ ci-scripts/run.sh
@@ -3,7 +3,7 @@ npm cache clean
 npm install
 
 node ci-scripts/generate-browserstack-json.js
-node_modules/nightwatch/bin/nightwatch tests/*.js -c browserstack.json
+node_modules/nightwatch/bin/nightwatch tests/*.js -c browserstack.json --retries $RETRIES
 
 #store exit code from tests
 success=$?
