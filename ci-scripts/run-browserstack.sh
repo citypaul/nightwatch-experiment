dd805f07320aa3045f0b02574c806b9f2f60a80b
Paul Hammond <paul.hammond@bbc.co.uk>
2016-04-24 13:09:30 +0100

    run scripts fail on lint error

--- ci-scripts/run-browserstack.sh
+++ ci-scripts/run-browserstack.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+set -e
+
+node_modules/eslint/bin/eslint.js tests
 node ci-scripts/generate-browserstack-json.js
 node_modules/nightwatch/bin/nightwatch tests/*.js -c browserstack.json --retries $RETRIES
 
--- ci-scripts/run.sh
+++ ci-scripts/run.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+set -e
+node_modules/eslint/bin/eslint.js tests
 node_modules/nightwatch/bin/nightwatch tests/*.js -c nightwatch.json --retries 5 --env chrome
