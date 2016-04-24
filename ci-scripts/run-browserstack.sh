67eda12b7d61870322c4150c276b2a449f0d35ac
Paul Hammond <paul.hammond@bbc.co.uk>
2016-04-24 16:22:01 +0100

    experimenting with browserstack local

--- ci-scripts/run-browserstack.sh
+++ ci-scripts/run-browserstack.sh
@@ -2,6 +2,13 @@
 set -e
 
 node_modules/eslint/bin/eslint.js tests
+
+curl -O https://s3-eu-west-1.amazonaws.com/live-sport-jenkins-master-infr-s3bucket-9evb9cmf37pg/BrowserStackLocal-linux-x64-3.8.zip
+unzip -o BrowserStackLocal-linux-x64-3.8.zip
+
+./BrowserStackLocal $BROWSERSTACK_KEY -forcelocal >/dev/null 2>&1 &
+
+
 node ci-scripts/generate-browserstack-json.js
 node_modules/nightwatch/bin/nightwatch tests/*.js -c browserstack.json --retries $RETRIES
 
